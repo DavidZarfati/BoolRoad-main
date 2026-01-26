212cfb0953b20608500d8fe4cf27b13cf3c2846e
DavidZarfati <zarda2001@gmail.com>
2026-01-26 16:44:59 +0100

    feat:card viaggi e generazione lista viaggi completate
    Co-authored-by: wissembrahem <wissembrahem@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,24 @@ function App() {
   return (
     <>
       <h1>Benvenuti dai los cavallos hermanos</h1>
-      <div className="card mb-3">
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img src={"/image/" + travels[0].image} className="img-fluid rounded-start" alt={travels[0].name} />
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">{travels[0].name}</h5>
-              <p className="card-text">{travels[0].description}</p>
-              <p className="card-text">from: {travels[0].initial_date} to: {travels[0].final_date} </p>
-              <p className="card-text"><small className="text-muted">travel created at : {travels[0].created_at}</small></p>
+      {travels.map((travel) => (
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src={"/image/" + travel.image} className="img-fluid rounded-start" alt={travel.name} />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">{travel.name}</h5>
+                <p className="card-text">{travel.description}</p>
+                <p className="card-text">from: {travel.initial_date} to: {travel.final_date} </p>
+                <p className="card-text"><small className="text-muted">travel created at : {travel.created_at}</small></p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
+
 
 
     </>
