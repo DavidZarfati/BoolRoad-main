7070d22e7626c7c8759d154c745595a1be502268
DavidZarfati <zarda2001@gmail.com>
2026-01-27 10:01:26 +0100

    feat: modifica card, con aggiunta bottone per mostrare e nascondere informazioni
    Co-authored-by: wissembrahem <wissembrahem@users.noreply.github.com>
    Co-authored-by: Ilaria Motta <ilaria.motta93@gmail.com

--- src/components/TravelersCard.jsx
+++ src/components/TravelersCard.jsx
@@ -1,27 +1,37 @@
 import travelersArray from "../../travelers.js"
-
+import React, { useState } from 'react';
 
 export default function TravelersCard({ traveler }) {
+    const [isVisible, setIsVisible] = useState(false);
     if (!traveler) return null;
     return (
-        <div className="d-flex flex-wrap gap-3">
-            <div className="card" style={{ width: "18rem" }} key={traveler.id}>
-                <img src={"/image/" + traveler.img} className="img-fluid rounded-start" alt={traveler.name} />
-                <div className="card-body">
-                    <h5 className="card-title">
-                        {traveler.name} {traveler.surname}
-                    </h5>
-                    <p className="card-text">
-                        <strong>Email:</strong> {traveler.email}
-                    </p>
-                    <p className="card-text">
-                        <strong>Telefono:</strong> {traveler.phone_number}
-                    </p>
-                    <p className="card-text">
-                        <strong>CF:</strong> {traveler.CF}
-                    </p>
+        <>
+            <div className="d-flex flex-wrap gap-3">
+                <div className="card" style={{ width: "18rem" }} key={traveler.id}>
+                    <img src={"/image/" + traveler.img} className="img-fluid rounded-start" alt={traveler.name} />
+                    <div className="card-body">
+                        <h5 className="card-title">
+                            {traveler.name} {traveler.surname}
+                        </h5>
+
+                        <button onClick={() => setIsVisible(!isVisible)}>
+                            {isVisible ? 'Nascondi' : 'Mostra'} Informazioni Personali
+                        </button>
+                        {isVisible && (<>                                 <p className="card-text">
+                            <strong>Email:</strong> {traveler.email}
+                        </p>
+                            <p className="card-text">
+                                <strong>Telefono:</strong> {traveler.phone_number}
+                            </p>
+                            <p className="card-text">
+                                <strong>CF:</strong> {traveler.CF}
+                            </p>
+                        </>
+
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
