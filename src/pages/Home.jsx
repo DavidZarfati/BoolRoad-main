f4bec284fd001dabe9743d3fab40aba259bce4c7
DavidZarfati <zarda2001@gmail.com>
2026-01-28 10:32:28 +0100

    feat:aggiunto bottone per form e migliorie varie
    Co-authored-by: Ilaria Motta <ilaria.motta93@gmail.com>
    Co-authored-by: Martina Giammillari <martinagiammillari@users.noreply.github.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,7 +42,7 @@ export default function Home() {
                         // console.log(resp.data)
                     })
             })
-
+        setLoadForm(false)
 
 
 
@@ -69,8 +69,29 @@ export default function Home() {
             })
     }, [])
 
+    const [loadForm, setLoadForm] = useState(false);
+
+    function resetForm() {
+        setFormData({
+            name: '',
+            description: '',
+            initial_date: '',
+            final_date: '',
+            image: null
+        });
+        // Reset file input manually
+        if (event.target && event.target.image) {
+            event.target.image.value = '';
+        }
+        setLoadForm(false)
+    }
+
+
+
+
     return (
         <>
+            <div className="sm_mainheader"> <button className="btn btn-primary" onClick={() => setLoadForm(true)}>Aggiungi viaggio </button> </div>
             <main className="app-container p-5 mx-auto my-5">
                 {travels.map((travel) => (
                     <Link
@@ -95,79 +116,86 @@ export default function Home() {
                 ))}
 
             </main>
-            <form className="mx-5" onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label">
-                        Destinazione
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="name"
-                        name="name"
-                        aria-describedby="emailHelp"
-                        onChange={handleChange}
-                        value={userInputData.name}
-                    />
-                    <div id="emailHelp" className="form-text">
-                        Inserisci qui la destinazione del nuovo viaggio
-                    </div>
+            {loadForm &&
+                <div className="sm_form">
+                    <form className="mx-5" onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                            <label htmlFor="name" className="form-label">
+                                Destinazione
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="name"
+                                name="name"
+                                aria-describedby="emailHelp"
+                                onChange={handleChange}
+                                value={userInputData.name}
+                            />
+                            <div id="emailHelp" className="form-text">
+                                Inserisci qui la destinazione del nuovo viaggio
+                            </div>
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="description" className="form-label">
+                                Descrizione del viaggio
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="description"
+                                name="description"
+                                onChange={handleChange}
+                                value={userInputData.description}
+                            />
+                            <label className="my-4 py-2">Quando inizia il viaggio?
+                                <input type="date" name="initial_date" onChange={handleChange} value={userInputData.initial_date} className="date-pill shadow-sm py-2" />
+                            </label>
+                            <br />
+                            <label className="my-4 py-2">Quando finisce il viaggio?
+                                <input type="date" name="final_date" onChange={handleChange} value={userInputData.final_date} className="date-pill shadow-sm py-2" />
+                            </label>
+                        </div>
+
+                        <div className="mb-3">
+
+                            <label className="form-label">
+                                Carica immagine
+                            </label>
+
+                            <input
+                                type="file"
+                                name="image"
+                                accept="image/*"
+                                className="form-control"
+                                onChange={handleChange}
+                            />
+
+                        </div>
+
+                        {/* Anteprima */}
+                        {userInputData.image && (
+                            <img
+                                src={URL.createObjectURL(userInputData.image)}
+                                alt="preview"
+                                width="200"
+                                className="mb-3"
+                            />
+                        )}
+
+
+
+
+                        <button type="submit" className="header-link border-none">
+                            Submit
+                        </button>
+                        <button onClick={resetForm} className="btn btn-warning ms-3">
+                            Close
+                        </button>
+                    </form>
                 </div>
-
-                <div className="mb-3">
-                    <label htmlFor="description" className="form-label">
-                        Descrizione del viaggio
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="description"
-                        name="description"
-                        onChange={handleChange}
-                        value={userInputData.description}
-                    />
-                    <label className="my-4 py-2">Quando inizia il viaggio?
-                        <input type="date" name="initial_date" onChange={handleChange} value={userInputData.initial_date} className="date-pill shadow-sm py-2" />
-                    </label>
-                    <br />
-                    <label className="my-4 py-2">Quando finisce il viaggio?
-                        <input type="date" name="final_date" onChange={handleChange} value={userInputData.final_date} className="date-pill shadow-sm py-2" />
-                    </label>
-                </div>
-
-                <div className="mb-3">
-
-                    <label className="form-label">
-                        Carica immagine
-                    </label>
-
-                    <input
-                        type="file"
-                        name="image"
-                        accept="image/*"
-                        className="form-control"
-                        onChange={handleChange}
-                    />
-
-                </div>
-
-                {/* Anteprima */}
-                {userInputData.image && (
-                    <img
-                        src={URL.createObjectURL(userInputData.image)}
-                        alt="preview"
-                        width="200"
-                        className="mb-3"
-                    />
-                )}
-
-
-
-
-                <button type="submit" className="header-link border-none">
-                    Submit
-                </button>
-            </form>
+            }
         </>
     );
 }
